4390560e0fc511a9e0e3cb4a54f8ce52a2879815
Miguel <mi_lmc@hotmail.com>
2026-02-03 11:33:21 -0300

    feat: setup dockerfile and include app into docker compose

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -2,7 +2,7 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('database', () => ({
   host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT, 10) || 5432,
+  port: parseInt(process.env.DB_PORT || '5432', 10),
   username: process.env.DB_USER || 'auth_user',
   password: process.env.DB_PASSWORD || 'auth_password',
   database: process.env.DB_NAME || 'auth_system',

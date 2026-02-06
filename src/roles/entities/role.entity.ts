7eb0727611a016af438b924ec802bb8282adf2ce
Miguel <mi_lmc@hotmail.com>
2026-02-06 09:12:54 -0300

    feat: enhance role entity to include many-to-many relationship with permissions

--- src/roles/entities/role.entity.ts
+++ src/roles/entities/role.entity.ts
@@ -4,7 +4,10 @@ import {
   Column,
   CreateDateColumn,
   UpdateDateColumn,
+  ManyToMany,
+  JoinTable,
 } from 'typeorm';
+import { Permission } from '../../permissions/entities/permission.entity';
 
 @Entity('roles')
 export class Role {
@@ -16,6 +19,14 @@ export class Role {
 
   @Column({ nullable: true })
   description: string;
+
+  @ManyToMany(() => Permission, { eager: true })
+  @JoinTable({
+    name: 'role_permissions',
+    joinColumn: { name: 'role_id', referencedColumnName: 'id' },
+    inverseJoinColumn: { name: 'permission_id', referencedColumnName: 'id' },
+  })
+  permissions: Permission[];
 
   @CreateDateColumn({ name: 'created_at' })
   createdAt: Date;

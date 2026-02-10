08e4977a5b6f238e83d801c2e77fceac76aab2f3
Miguel <mi_lmc@hotmail.com>
2026-02-10 17:20:56 -0300

    feat: setup auth module with all dependencies

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,32 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { JwtStrategy } from './strategies/jwt.strategy';
 import { PasswordService } from './services/password.service';
+import { User } from '../users/entities/user.entity';
+import { RefreshToken } from './entities/refresh-token.entity';
 
 @Module({
-  providers: [PasswordService],
-  exports: [PasswordService],
+  imports: [
+    TypeOrmModule.forFeature([User, RefreshToken]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET') || 'secret-key',
+        signOptions: {
+          expiresIn: configService.get('JWT_EXPIRES_IN') || '15m',
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, JwtStrategy, PasswordService],
+  exports: [AuthService, PasswordService],
 })
 export class AuthModule {}
